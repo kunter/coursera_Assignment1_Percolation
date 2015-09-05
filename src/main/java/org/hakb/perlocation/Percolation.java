1c91d6f38e72bdfb43d6a34857c44e09568f7cd2
kunter <YChernyatin@gmail.com>
2015-09-06 02:06:47 +0300

    added methods to check is there an open site  nearby

--- src/main/java/org/hakb/perlocation/Percolation.java
+++ src/main/java/org/hakb/perlocation/Percolation.java
@@ -1,9 +1,5 @@
 package org.hakb.perlocation;
 
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-
 public class Percolation {
     boolean[][] grid;
 
