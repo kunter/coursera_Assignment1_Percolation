4d2175881f2c54fe3437f0842f6089f3a28ce721
kunter <YChernyatin@gmail.com>
2015-09-06 01:17:10 +0300

    add Percolation data type

--- src/main/java/org/hakb/Main.java
+++ src/main/java/org/hakb/Main.java
@@ -1,8 +1,10 @@
 package org.hakb;
-
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Main {
     public static void main(String args[]) {
-        
+
     }
 }
---
+++ src/main/java/org/hakb/perlocation/Percolation.java
@@ -0,0 +1,36 @@
+package org.hakb.perlocation;
+
+/**
+ * Created by Администратор on 06.09.2015.
+ */
+public class Percolation {
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N) {
+        throw new UnsupportedOperationException("Not ready yet");
+    }
+
+    // open site (row i, column j) if it is not open already
+    public void open(int i, int j) {
+        throw new UnsupportedOperationException("Not ready yet");
+    }
+
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+        throw new UnsupportedOperationException("Not ready yet");
+    }
+
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j) {
+        throw new UnsupportedOperationException("Not ready yet");
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        throw new UnsupportedOperationException("Not ready yet");
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException("Not ready yet");
+    }
+}
