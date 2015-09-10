d6962342e61ed3d5f50bbc12c5b9ec7748ea701a
YChernyatin <chernyatin@protei.ru>
2015-09-10 14:13:17 +0400

    Add Perlocation Stats

---
+++ src/main/java/org/hakb/perlocation/PercolationStats.java
@@ -0,0 +1,59 @@
+package org.hakb.perlocation;
+
+public class PercolationStats {
+
+    /**
+     * perform T independent experiments on an N-by-N grid
+     *
+     * @param N
+     * @param T
+     */
+    public PercolationStats(int N, int T) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * sample mean of percolation threshold
+     *
+     * @return
+     */
+    public double mean() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * sample standard deviation of percolation threshold
+     *
+     * @return
+     */
+    public double stddev() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * low  endpoint of 95% confidence interval
+     *
+     * @return
+     */
+    public double confidenceLo() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * high endpoint of 95% confidence interval
+     *
+     * @return
+     */
+    public double confidenceHi() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * test client (described below)
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
